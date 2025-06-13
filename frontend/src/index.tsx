8ed64a94c81b3b9f70f7dbf8c6749ea439e4ae8a
Juan Rios <cf-incubator@usa.com>
2025-06-12 19:30:55 -0500

    Eliminado web-vitals y sus referencias

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -11,6 +10,4 @@ root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
-);
-
-reportWebVitals(); 
+); 
