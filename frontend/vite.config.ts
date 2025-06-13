ff93370df7808564d926cd258bad6abf303cd8c3
Juan Rios <cf-incubator@usa.com>
2025-06-12 19:39:37 -0500

    Actualizada configuración de Vite para manejar hoist-non-react-statics

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,11 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
-      external: ['deepmerge', 'tiny-warning'],
+      external: [
+        'deepmerge',
+        'tiny-warning',
+        'hoist-non-react-statics'
+      ],
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
@@ -22,12 +26,17 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ['react', 'react-dom', '@mui/material', '@mui/icons-material'],
-    exclude: ['deepmerge', 'tiny-warning']
+    exclude: [
+      'deepmerge',
+      'tiny-warning',
+      'hoist-non-react-statics'
+    ]
   },
   resolve: {
     mainFields: ['module', 'jsnext:main', 'jsnext', 'browser', 'main'],
     alias: {
-      'tiny-warning': 'tiny-warning/dist/tiny-warning.esm.js'
+      'tiny-warning': 'tiny-warning/dist/tiny-warning.esm.js',
+      'hoist-non-react-statics': 'hoist-non-react-statics/dist/hoist-non-react-statics.esm.js'
     }
   }
 }) 
